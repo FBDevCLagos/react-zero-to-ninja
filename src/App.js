2e93f233920af2f401c527e1431ea173d483419e
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2018-09-12 12:28:07 +0100

    context api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Todo from './containers/Todo'
+import Theme from './containers/context/Theme'
 
 class App extends Component {
   render () {
@@ -7,7 +9,12 @@ class App extends Component {
       <div id="root-container">
         <h1 id="__welcome-header">Welcome!</h1>
         <hr />
-        <Todo />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={Todo} />
+            <Route path="/context" exact component={Theme} />
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
---
+++ src/containers/context/Theme.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+// Context lets us pass a value deep into the component tree
+// without explicitly threading it through every component.
+// Create a context for the current theme (with "light" as the default).
+const ThemeContext = React.createContext('orange')
+
+class Theme extends Component {
+  render () {
+    // Use a Provider to pass the current theme to the tree below.
+    // Any component can read it, no matter how deep it is.
+    // In this example, we're passing "green" as the current value.
+    return (
+      <ThemeContext.Provider value="green">
+        <Toolbar />
+      </ThemeContext.Provider>
+    )
+  }
+}
+
+// A component in the middle doesn't have to
+// pass the theme down explicitly anymore.
+function Toolbar (props) {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  )
+}
+
+function ThemedButton (props) {
+  // Use a Consumer to read the current theme context.
+  // React will find the closest theme Provider above and use its value.
+  // In this example, the current theme is "dark".
+  return (
+    <ThemeContext.Consumer>
+      {theme => {
+        return (
+          <button {...props} style={{ backgroundColor: theme }}>
+            The Context Button
+          </button>
+        )
+      }}
+    </ThemeContext.Consumer>
+  )
+}
+
+export default Theme
